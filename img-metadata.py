ff81ac7a788a39d7d7b483e46b03141a155f46f0
Syre Collins <dannyperfect235@gmail.com>
2025-07-10 16:20:40 -0700

    Update img-metadata.py

--- img-metadata.py
+++ img-metadata.py
@@ -1,32 +1,38 @@
 import sys
 import os
 import json
-from PIL import Image, ExifTags, ImageCms, ImageStat
+from PIL import Image, ImageCms, ImageStat
 import imagehash
+import piexif
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 from io import BytesIO
 
 app = FastAPI(title="Image Metadata Extractor")
 
-def get_gps_info(exif_data):
-    gps_info = exif_data.get('GPSInfo')
-    if not gps_info:
-        return None
-    def convert_to_degrees(value):
-        d, m, s = value
-        return d[0]/d[1] + m[0]/m[1]/60 + s[0]/s[1]/3600
-    lat = convert_to_degrees(gps_info[2])
-    if gps_info[1] == 'S':
-        lat = -lat
-    lon = convert_to_degrees(gps_info[4])
-    if gps_info[3] == 'W':
-        lon = -lon
-    return {
-        "latitude": lat,
-        "longitude": lon,
-        "google_maps": f"https://www.google.com/maps/search/?api=1&query={lat},{lon}"
-    }
+def get_gps_info(gps_ifd):
+    try:
+        def convert_to_degrees(value):
+            d, m, s = value
+            return d[0]/d[1] + m[0]/m[1]/60 + s[0]/s[1]/3600
+
+        lat = convert_to_degrees(gps_ifd[2])
+        if gps_ifd[1] == b'S':
+            lat = -lat
+        lon = convert_to_degrees(gps_ifd[4])
+        if gps_ifd[3] == b'W':
+            lon = -lon
+
+        alt = gps_ifd[6][0] / gps_ifd[6][1] if 6 in gps_ifd else None
+
+        return {
+            "latitude": lat,
+            "longitude": lon,
+            "altitude": alt,
+            "google_maps": f"https://www.google.com/maps/search/?api=1&query={lat},{lon}"
+        }
+    except:
+        return {}
 
 def extract_metadata(image_file):
     metadata = {}
@@ -37,6 +43,7 @@ def extract_metadata(image_file):
     metadata['filename'] = getattr(image_file, 'name', 'unknown')
     metadata['file_size_bytes'] = os.fstat(image_file.fileno()).st_size if hasattr(image_file, 'fileno') else None
 
+    # ICC Profile
     icc_profile = image.info.get('icc_profile')
     if icc_profile:
         try:
@@ -47,36 +54,41 @@ def extract_metadata(image_file):
     else:
         metadata['icc_profile'] = None
 
-    exif_data = {}
+    # EXIF Metadata
     exif_summary = {}
     gps_data = {}
     try:
-        exif_raw = image._getexif()
-        if exif_raw:
-            for tag, value in exif_raw.items():
-                decoded = ExifTags.TAGS.get(tag, tag)
-                exif_data[decoded] = value
-            gps_info = get_gps_info(exif_data)
-            if gps_info:
-                gps_data = gps_info
-            # Extract summary EXIF fields
-            exif_summary['camera_make'] = exif_data.get('Make')
-            exif_summary['camera_model'] = exif_data.get('Model')
-            exif_summary['iso'] = exif_data.get('ISOSpeedRatings')
-            exif_summary['exposure_time'] = exif_data.get('ExposureTime')
-            exif_summary['aperture'] = exif_data.get('FNumber')
-            exif_summary['focal_length'] = exif_data.get('FocalLength')
-            exif_summary['date_taken'] = exif_data.get('DateTimeOriginal')
-    except:
-        pass
+        if "exif" in image.info:
+            exif_dict = piexif.load(image.info["exif"])
+            zeroth_ifd = exif_dict["0th"]
+            exif_ifd = exif_dict["Exif"]
+            gps_ifd = exif_dict["GPS"]
+
+            exif_summary['camera_make'] = zeroth_ifd.get(piexif.ImageIFD.Make, b'').decode('utf-8', 'ignore')
+            exif_summary['camera_model'] = zeroth_ifd.get(piexif.ImageIFD.Model, b'').decode('utf-8', 'ignore')
+            exif_summary['iso'] = exif_ifd.get(piexif.ExifIFD.ISOSpeedRatings)
+            exif_summary['exposure_time'] = exif_ifd.get(piexif.ExifIFD.ExposureTime)
+            exif_summary['aperture'] = exif_ifd.get(piexif.ExifIFD.FNumber)
+            exif_summary['focal_length'] = exif_ifd.get(piexif.ExifIFD.FocalLength)
+            exif_summary['date_taken'] = exif_ifd.get(piexif.ExifIFD.DateTimeOriginal, b'').decode('utf-8', 'ignore')
+
+            if gps_ifd:
+                gps_data = get_gps_info(gps_ifd)
+        else:
+            exif_summary['info'] = "No EXIF data found in image."
+    except Exception as e:
+        exif_summary['error'] = str(e)
+
     metadata['exif'] = exif_summary
     metadata['gps'] = gps_data
 
+    # Perceptual Hash
     try:
         metadata['perceptual_hash'] = str(imagehash.phash(image))
     except:
         metadata['perceptual_hash'] = None
 
+    # Histogram Data
     try:
         stat = ImageStat.Stat(image.convert('RGB'))
         metadata['histogram_mean'] = stat.mean
@@ -89,7 +101,7 @@ def extract_metadata(image_file):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python extract_metadata.py <image_path>")
+        print("Usage: python img-metadata.py <image_path>")
         sys.exit(1)
     image_path = sys.argv[1]
     if not os.path.exists(image_path):
